0d2b7b60d8e17c7a66874b045dae0884eb32de22
hertafranca <hertafranca.tech@gmail.com>
2020-04-17 14:26:31 -0300

    data change for local variable

--- MaratonaDev3/server.js
+++ MaratonaDev3/server.js
@@ -1,66 +1,59 @@
-const express = require("express")
-const server = express()
+const express = require("express");
+const { resolve } = require("path");
+const server = express();
 
-server.use(express.static('public'))
+server.use(express.static(resolve(__dirname, "public")));
 
-server.use(express.urlencoded({extended:true}))
+server.use(express.urlencoded({ extended: true }));
 
-const Pool = require('pg').Pool
-const db = new Pool({
-  user:'postgres',
-  password:'1234',
-  host:'localhost',
-  port:5432,
-  database:'donate',
+const donors = [
+  { name: "Herta França", email: "hertafranca.tech@gmail.com", blood: "O+" },
+];
 
-})
- 
-const nunjucks = require("nunjucks")
-nunjucks.configure("./", {
-  express:server
-})
+const nunjucks = require("nunjucks");
+nunjucks.configure(__dirname, {
+  express: server,
+});
 
+server.get("/", function (req, res) {
+  // db.query("SELECT * FROM donors", function (err, result) {
+  //   if (err) {
+  //     res.send("database error.");
+  //   }
 
-server.get("/", function(req,res) {
+  //   const donors = result.rows;
+  //   return res.render("index.html", { donors });
+  // });
 
-  db.query("SELECT * FROM donors", function (err, result) {
-    if (err) {
-      res.send("database error.")
-    } 
+  return res.render("index.html", { donors });
+});
 
-    const donors = result.rows;
-    return res.render("index.html", {donors})
-  })
-})
+server.post("/", function (req, res) {
+  const name = req.body.name;
+  const email = req.body.email;
+  const blood = req.body.blood;
 
-server.post("/", function(req,res) {
-  const name = req.body.name
-  const email = req.body.email
-  const blood = req.body.blood
-
-  console.log(name,email,blood);
+  console.log(name, email, blood);
 
   if (name == "" || email == "" || blood == "") {
-    return res.send("All fields must be filled out.")
+    return res.send("All fields must be filled out.");
   }
 
-  const query = `
-    INSERT INTO donors ("name", "email", "blood")
-    VALUES ($1, $2, $3)`
+  // const query = `
+  //   INSERT INTO donors ("name", "email", "blood")
+  //   VALUES ($1, $2, $3)`;
 
-  const values = [name, email, blood]
+  // const values = [name, email, blood];
 
-  db.query(query, values, function(err) {
-    if (err) {
-      console.log(err);
-      return res.send("database error")
-    }
-    return res.redirect("/")
-  })
+  // db.query(query, values, function (err) {
+  //   if (err) {
+  //     console.log(err);
+  //     return res.send("database error");
+  //   }
+  //   return res.redirect("/");
+  // });
+  donors.push({ name, email, blood });
+  return res.redirect("/");
+});
 
-})
-
-server.listen(3000, function() {
-  console.log("iniciei o servidor.")
-}) 
- 
+server.listen(4444);
